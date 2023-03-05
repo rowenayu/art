77efc2879582a79dc0fbb07ea5ed5065deb04222
rowenayu <rowenayu96@gmail.com>
2023-03-05 13:27:21 +1100

    social icon

--- client/src/assets/wrappers/ContactMe.js
+++ client/src/assets/wrappers/ContactMe.js
@@ -1,12 +1,65 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.section`
-  .form {
-    width: 80%;
-    padding: 12px 20px;
-    /* margin: 8px 0;
-    box-sizing: border-box; */
-  }
+  body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(45deg, greenyellow, dodgerblue);
+  font-family: "Sansita Swashed", cursive;
+}
+  .center {
+  position: relative;
+  padding: 50px 50px;
+  background: #fff;
+  border-radius: 10px;
+}
+  .center .inputbox {
+  position: relative;
+  width: 300px;
+  height: 50px;
+  margin-bottom: 50px;
+}
+.center .inputbox input {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border: 2px solid #000;
+  outline: none;
+  background: none;
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 1.2em;
+}
+.center .inputbox textarea {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border: 2px solid #000;
+  outline: none;
+  background: none;
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 1.2em;
+}
+.center .inputbox [type="button"] {
+  width: 50%;
+  background: dodgerblue;
+  color: #fff;
+  border: #fff;
+}
+.center .inputbox:hover [type="button"] {
+  background: linear-gradient(45deg, greenyellow, dodgerblue);
+}
+.center .inputbox:last-child {
+  margin-bottom: 0;
+}
 `
 
 export default Wrapper
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import Wrapper from "../assets/wrappers/Navbar"
 import {FaAlignLeft} from 'react-icons/fa'
 import Logo from './Logo'
 import { useAppContext } from "../context/appContext"
+import { SocialIcon } from 'react-social-icons';
 
 const Navbar = () => {
   const {toggleSidebar} = useAppContext()
@@ -13,6 +14,9 @@ const Navbar = () => {
           </button>
           <div>
             <Logo/>
+          </div>
+          <div>
+            <SocialIcon url="https://www.linkedin.com/in/rowena-yu-4962b4100/" />
           </div>
         </div>
     </Wrapper>
